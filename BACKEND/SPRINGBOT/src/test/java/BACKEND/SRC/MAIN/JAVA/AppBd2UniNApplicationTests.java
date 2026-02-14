23fc92f18f087ba6d28bac7a803f9ab5800a6424
Jesus <jcampoy21@campusuninunez.edu.co>
2026-02-14 01:16:20 -0500

    5TO commit

---
+++ BACKEND/SPRINGBOT/src/test/java/BACKEND/SRC/MAIN/JAVA/AppBd2UniNApplicationTests.java
@@ -0,0 +1,13 @@
+package BACKEND.SRC.MAIN.JAVA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBd2UniNApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
